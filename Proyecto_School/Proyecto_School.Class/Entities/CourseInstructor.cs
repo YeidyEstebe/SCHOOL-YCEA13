fc60e2cfda3d52eb2c8e6a98e7042276e7c414a4
YeidyEstebe <ChanelEstebe@gmail.com>
2024-02-10 20:48:06 +0100

    Update CourseInstructor.cs
    se crearon los Atributos y Propiedades en la clase "CourseInstructor.cs". Representa la tabla CourseInstructor de la BD School.

--- Proyecto_School/Proyecto_School.Class/Entities/CourseInstructor.cs
+++ Proyecto_School/Proyecto_School.Class/Entities/CourseInstructor.cs
@@ -6,7 +6,27 @@ namespace Proyecto_School.Class.Entities
     public class CourseInstructor
     {
         
+     #region "Atributos"
+
+     public int _CourseID;
+     public int _PersonID;
      
+     #endregion
+
+     #region "Propiedades"
+     public int CourseID
+     {
+        get { return _CourseID; }
+        set { _CourseID = value; }
+     }
+          public int PersonID
+     {
+        get { return _PersonID; }
+        set { _PersonID = value; }
+     }
+     
+     
+     #endregion
 
     }   
 }    
