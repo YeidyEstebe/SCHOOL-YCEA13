9d2b22ead2afc877ce870143917b860ce5ff1776
YeidyEstebe <ChanelEstebe@gmail.com>
2024-02-10 20:31:09 +0100

    School-Ycea13
    School-Ycea13
    
    -Se creo un repository en GitHub.
    -Se clono en Visual Studio.
    -se creo un nuevo proyecto en blanco "PROYECTO_SCHOOL" dentro de Visual Studio y se guardo el repository clonado.
    -Se cerro visual studio y se abrio la solucion del proyecto creado.
    -Se creo dentro de la solucion el proyecto "Proyecto_School.Class" tipo Library Class.
    -Se creo dentro la libreria "Proyecto_School.Class" un folder "Entities"para almacenar todas las clases del negocio, esta clases representean en la app la tablas de la BD School.
    -Se creo en el folder "Entities" la Clase "Course.cs",  dentro de la misma se crearon los Atributos y Propiedades. Representa la tabla Course de la BD School.
    -Se creo dentro la libreria "Proyecto_School.Class" un folder  "DataObjects". Aun no se declara correctamente su uso.
    -Se creo en el folder "Entities" la Clase "CourseInstructor.cs" Aun no se agregan los Atributos y Propiedades. Representa la tabla CourseInstructor de la BD School.

---
+++ Proyecto_School/Proyecto_School.Class/Entities/Course.cs
@@ -0,0 +1,96 @@
+﻿
+
+
+namespace Proyecto_School.Class.Entities
+{
+    public class Course
+    {
+        #region "Atributos"
+
+        private int _CourseID;
+        private string? _Title;
+        private int _Credits;
+        private int _DepartmentID;
+        private DateTime _CreationDate;
+        private DateTime? _ModifyDate;
+        private int? _CreationUser;
+        private int? _UserMod;
+        private int? _UserDeleted;
+        private DateTime? _DeletedDate;
+        private bool _Deleted;
+        #endregion
+
+        #region "Propiedades"
+
+        
+        public int CourseID
+        {
+            get { return _CourseID; }
+            set { _CourseID = value; }
+        }
+                
+        public string? Title
+        {
+            get { return _Title; }
+            set { _Title = value; }
+        }
+        
+        public int Credits
+        {
+            get { return _Credits; }
+            set { _Credits = value; }
+        }
+        
+        public int DepartmentID
+        {
+            get { return _DepartmentID; }
+            set { _DepartmentID = value; }
+        }
+        
+        public DateTime CreationDate
+        {
+            get { return _CreationDate; }
+            set { _CreationDate = value; }
+        }
+        
+        public DateTime? ModifyDate
+        {
+            get { return _ModifyDate; }
+            set { _ModifyDate = value; }
+        }
+        
+        public int? CreationUser
+        {
+            get { return _CreationUser; }
+            set { _CreationUser = value; }
+        }
+
+        public int? UserMod
+        {
+            get { return _UserMod; }
+            set { _UserMod = value; }
+        }
+        
+        public int? UserDeleted
+        {
+            get { return _UserDeleted; }
+            set { _UserDeleted = value; }
+        }
+        
+        public DateTime? DeletedDate
+        {
+            get { return _DeletedDate; }
+            set { _DeletedDate = value; }
+        }
+       
+        public bool Deleted
+        {
+            get { return _Deleted; }
+            set { _Deleted = value; }
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ Proyecto_School/Proyecto_School.Class/Entities/CourseInstructor.cs
@@ -0,0 +1,12 @@
+
+
+
+namespace Proyecto_School.Class.Entities
+{
+    public class CourseInstructor
+    {
+        
+     
+
+    }   
+}    
