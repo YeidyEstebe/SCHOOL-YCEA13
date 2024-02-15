3e656ff4a4bd4042e52208c14b67e3918381b6d6
YeidyEstebe <ChanelEstebe@gmail.com>
2024-02-14 22:56:52 -0400

    creacion de clase
    se creo la clase OnlineCourse

---
+++ Proyecto_School/Proyecto_School.Class/Entities/OnlineCourse.cs
@@ -0,0 +1,39 @@
+namespace Proyecto_School.class.Entities
+{
+    public class OnlineCourse
+    {
+
+        #region "Atributos"
+
+            public int _CourseID
+            public strin? _URL
+        
+        #endregion
+
+
+        #region "Propiedades"
+
+       
+        public int CourseID
+        {
+            get { return _CourseID; }
+            set { _CourseID = value; }
+        }
+        
+        public strin? URL
+        {
+            get {return CourseID}
+            set {CourseID = value}
+
+        }
+
+        #endregion 
+
+    
+
+    }
+
+
+
+
+}
