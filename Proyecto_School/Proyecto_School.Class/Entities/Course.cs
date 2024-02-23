b4fe9662cd56d16f1edee5a6e53acaefe006a585
YeidyEstebe <ChanelEstebe@gmail.com>
2024-02-22 21:46:00 -0400

    Actualizacion de clase.
    Se eliminaron los campos repetidos de las clases (Course.cs-Departments.cs ) los mismos seran estraidos de la clase Abstracta.

--- Proyecto_School/Proyecto_School.Class/Entities/Course.cs
+++ Proyecto_School/Proyecto_School.Class/Entities/Course.cs
@@ -11,13 +11,7 @@ namespace Proyecto_School.Class.Entities
         private string? _Title;
         private int _Credits;
         private int _DepartmentID;
-        private DateTime _CreationDate;
-        private DateTime? _ModifyDate;
-        private int? _CreationUser;
-        private int? _UserMod;
-        private int? _UserDeleted;
-        private DateTime? _DeletedDate;
-        private bool _Deleted;
+        
         #endregion
 
         #region "Propiedades"
@@ -47,48 +41,6 @@ namespace Proyecto_School.Class.Entities
             set { _DepartmentID = value; }
         }
         
-        public DateTime CreationDate
-        {
-            get { return _CreationDate; }
-            set { _CreationDate = value; }
-        }
-        
-        public DateTime? ModifyDate
-        {
-            get { return _ModifyDate; }
-            set { _ModifyDate = value; }
-        }
-        
-        public int? CreationUser
-        {
-            get { return _CreationUser; }
-            set { _CreationUser = value; }
-        }
-
-        public int? UserMod
-        {
-            get { return _UserMod; }
-            set { _UserMod = value; }
-        }
-        
-        public int? UserDeleted
-        {
-            get { return _UserDeleted; }
-            set { _UserDeleted = value; }
-        }
-        
-        public DateTime? DeletedDate
-        {
-            get { return _DeletedDate; }
-            set { _DeletedDate = value; }
-        }
-       
-        public bool Deleted
-        {
-            get { return _Deleted; }
-            set { _Deleted = value; }
-        }
-
 
         #endregion
 
